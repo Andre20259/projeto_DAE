9c510a695fc2a058606db7b3234a84551938aebb
André Lopes <andreglopes2005@gmail.com>
2026-01-15 17:32:04 +0000

    beans for breakfast?? only in UK

---
+++ Projeto_DAE/src/main/java/pt/ipleiria/estg/dei/ei/dae/projeto_dae/entities/Colaborator.java
@@ -0,0 +1,6 @@
+package pt.ipleiria.estg.dei.ei.dae.projeto_dae.entities;
+
+import java.io.Serializable;
+
+public class Colaborator extends SuperUser implements Serializable {
+}
---
+++ Projeto_DAE/src/main/java/pt/ipleiria/estg/dei/ei/dae/projeto_dae/entities/Responsible.java
@@ -0,0 +1,6 @@
+package pt.ipleiria.estg.dei.ei.dae.projeto_dae.entities;
+
+import java.io.Serializable;
+
+public class Responsible extends SuperUser implements Serializable {
+}
