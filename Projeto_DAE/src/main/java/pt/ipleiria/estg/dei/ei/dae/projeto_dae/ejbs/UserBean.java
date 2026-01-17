3a6b8bc0a0e84f85637d2e1f43d224ec35884e22
Ana Margarida <2230700@my.ipleiria.pt>
2026-01-17 18:32:23 +0000

    Tags

--- Projeto_DAE/src/main/java/pt/ipleiria/estg/dei/ei/dae/projeto_dae/ejbs/UserBean.java
+++ Projeto_DAE/src/main/java/pt/ipleiria/estg/dei/ei/dae/projeto_dae/ejbs/UserBean.java
@@ -2,8 +2,12 @@ package pt.ipleiria.estg.dei.ei.dae.projeto_dae.ejbs;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import pt.ipleiria.estg.dei.ei.dae.projeto_dae.entities.User;
 
 public class UserBean {
     @PersistenceContext
     private EntityManager entityManager;
+    public User find(String username) {
+        return entityManager.find(User.class, username);
+    }
 }
--- Projeto_DAE/src/main/java/pt/ipleiria/estg/dei/ei/dae/projeto_dae/entities/Publication.java
+++ Projeto_DAE/src/main/java/pt/ipleiria/estg/dei/ei/dae/projeto_dae/entities/Publication.java
@@ -1,4 +1,13 @@
 package pt.ipleiria.estg.dei.ei.dae.projeto_dae.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+
+import java.util.List;
+
+@Entity
 public class Publication {
+
+    @ManyToMany(mappedBy = "publications")
+    private List<Tag> tags;
 }
