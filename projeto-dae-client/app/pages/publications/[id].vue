3e731cc584c668efa834bed6fbb4e734cabea9c5
Henrique Vilaça <2230707@my.ipleiria.pt>
2026-01-23 01:51:22 +0000

    i need to sleep

---
+++ projeto-dae-client/app/pages/publications/[id].vue
@@ -0,0 +1,14 @@
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const id = route.params.id
+
+console.log('Publication ID:', id)
+</script>
+
+<template>
+  <div>
+    <h1>Publication {{ id }}</h1>
+  </div>
+</template>
