f9a5350e55e197a084ddff62f93d3d7530621c51
Ana Margarida <2230700@my.ipleiria.pt>
2026-01-23 22:53:38 +0000

    Deploy

--- Projeto_DAE/src/main/java/pt/ipleiria/estg/dei/ei/dae/projeto_dae/ejbs/ConfigBean.java
+++ Projeto_DAE/src/main/java/pt/ipleiria/estg/dei/ei/dae/projeto_dae/ejbs/ConfigBean.java
@@ -36,20 +36,7 @@ public class ConfigBean {
     
     @EJB
     private CommentBean commentBean;
-    
-    
 
-    @EJB
-    private PublicationBean publicationBean;
-
-    @EJB
-    private CommentBean commentBean;
-
-    @EJB
-    private RatingBean ratingBean;
-
-    @EJB
-    private TagBean tagBean;
 
     private static final Logger logger = Logger.getLogger("ejbs.ConfigBean");
 
@@ -67,6 +54,10 @@ public class ConfigBean {
             tagBean.create("AI");
             tagBean.create("Technology");
             tagBean.create("Engineering");
+            tagBean.create("Science");
+            tagBean.create("HiddenTag");
+            var hiddenTag = tagBean.find("HiddenTag");
+            hiddenTag.setVisible(false);
 
             PublicationCreateDTO publicationCreateDTO = new PublicationCreateDTO();
             publicationCreateDTO.setTitle("Teste 2");
@@ -83,81 +74,61 @@ public class ConfigBean {
 
             commentBean.create("colab",1L,"Great publication!");
             ratingBean.create("colab",1L,5);
-        }
-        catch (Exception e){
-            logger.severe(e.getMessage());
-        }
-        try{
+
             colaboratorBean.create("deleted_user", "password", "Deletec User", "deleted@mail.pt");
             colaboratorBean.create("johndoe123", "password123", "JohnDoe", "test@mail.pt");
             colaboratorBean.create("maria", "password123", "Maria", "maria@mail.pt");
             colaboratorBean.create("manel", "password123", "Manel", "manel@mail.pt");
             colaboratorBean.create("raquel", "password123", "Raquel", "raquel@mail.pt");
-        }
-        catch (Exception e){
-            logger.severe(e.getMessage());
-        }
-        try{
-            responsibleBean.create("resp", "password123", "Responsavel", "resp1@mail.pt");
-        }
-        catch (Exception e){
-            logger.severe(e.getMessage());
-        }
 
-        try{
-            tagBean.create("Science");
-            tagBean.create("AI");
-            tagBean.create("HiddenTag");
-            var hiddenTag = tagBean.find("HiddenTag");
-            hiddenTag.setVisible(false);
-        }
-        catch (Exception e){
-            logger.severe(e.getMessage());
-        }
+            PublicationCreateDTO dto = new PublicationCreateDTO();
+            dto.setTitle("Primeira Publicação");
+            dto.setDescription("Publicação inicial");
+            dto.setArea("Software");
+            dto.setAuthors(List.of("admin"));
+            dto.setTags(List.of("Science"));
 
-        PublicationCreateDTO dto = new PublicationCreateDTO();
-        dto.setTitle("Primeira Publicação");
-        dto.setDescription("Publicação inicial");
-        dto.setArea("Software");
-        dto.setAuthors(List.of("admin"));
-        dto.setTags(List.of("Science"));
+            InputStream dummyFile =
+                    new ByteArrayInputStream("conteudo fake".getBytes());
 
-        InputStream dummyFile =
-                new ByteArrayInputStream("conteudo fake".getBytes());
-
-        PublicationCreateDTO hiddenDto = new PublicationCreateDTO();
-        hiddenDto.setTitle("Publicação Oculta");
-        hiddenDto.setDescription("Não deve aparecer no GET público");
-        hiddenDto.setArea("Software");
-        hiddenDto.setAuthors(List.of("admin"));
-        hiddenDto.setTags(List.of("AI"));
-
-        InputStream hiddenFile =
-                new ByteArrayInputStream("conteudo oculto".getBytes());
-
-        try{
             publicationBean.create("file.txt", dummyFile, dto);
+
+            PublicationCreateDTO dto2 = new PublicationCreateDTO();
+            dto.setTitle("Segunda Publicação");
+            dto.setDescription("Publicação ");
+            dto.setArea("Software");
+            dto.setAuthors(List.of("raquel"));
+            dto.setTags(List.of("Science"));
+
+            InputStream dummyFile2 =
+                    new ByteArrayInputStream("conteudo fake".getBytes());
+
+            publicationBean.create("file.txt", dummyFile2, dto2);
+
+            PublicationCreateDTO hiddenDto = new PublicationCreateDTO();
+            hiddenDto.setTitle("Publicação Oculta");
+            hiddenDto.setDescription("Não deve aparecer no GET público");
+            hiddenDto.setArea("Software");
+            hiddenDto.setAuthors(List.of("admin"));
+            hiddenDto.setTags(List.of("AI"));
+
+            InputStream hiddenFile =
+                    new ByteArrayInputStream("conteudo oculto".getBytes());
+
             var hiddenPub = publicationBean.create("hidden.txt", hiddenFile, hiddenDto);
             hiddenPub.setVisible(false);
-        }
-        catch (Exception e){
-            logger.severe(e.getMessage());
-        }
 
-        try{
+
             commentBean.create("johndoe123", 1L, "Good publication");
             commentBean.create("raquel", 1L, "Very informative");
             var hiddenComment =
                     commentBean.create("johndoe123", 1L, "Comentário oculto");
             hiddenComment.setVisible(false);
-        }
-        catch (Exception e){
-            logger.severe(e.getMessage());
-        }
 
-        try{
+
             ratingBean.create("johndoe123", 1L, 5);
             ratingBean.create("manel", 1L, 5);
+
 
         }
         catch (Exception e){
